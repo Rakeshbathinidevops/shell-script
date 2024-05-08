62160052e2e695d15c22e5f08b666d1eac9334d2
rakeshbathinidevops <rakeshbp2017@gmail.com>
2024-05-08 12:58:35 +0530

    message

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-USERID=$(id-u)  
+USERID=$(id -u)
 
 if [$USERID -ne 0]
 then
     echo "run with root access"
+    exit1
 else
     echo "you are super user"
 
 fi
 
 dnf install mysql -y
+
+echo "is script proceeding?"
