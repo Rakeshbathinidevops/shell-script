f91e8e930912f75cb64d8ee7541b4f5275c43e98
rakeshbathinidevops <rakeshbp2017@gmail.com>
2024-05-08 21:19:50 +0530

    message

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,16 +2,19 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$( echo $O | cut -d "." -f1 )
+SCRIPT_NAME=$(echo $O | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VERIFY(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 is failure"
+        echo "$2 is $R failure $N"
         exit 1
     else
-        echo "$2 is successful"
+        echo "$2 is $G successful $N"
     fi
 }
 
@@ -29,4 +32,8 @@ VERIFY $? "installing mysql"
 
 dnf install git -y &>>$LOGFILE
 VERIFY $? "installing git"
+
+dnf install dockerr -y &>>$LOGFILE
+VERIFY $? "installing docker"
+
 echo "is script proceeding?"
