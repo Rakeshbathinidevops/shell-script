f12ec68954765461f1fe274fa79739381a537ebe
rakeshbathinidevops <rakeshbp2017@gmail.com>
2024-05-08 19:55:40 +0530

    message

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $O | cut -d "." f1)
+LOGFILE=/tem/$SCRIPT_NAME_$TIMESTAMP.log
 
 VERIFY(){
     if [ $1 -ne 0 ]
     then
         echo "$2 is failure"
+        exit 1
     else
         echo "$2 is successful"
     fi
@@ -20,9 +24,9 @@ else
 
 fi
 
-dnf install mysql -y
+dnf install mysql -y | &>>$LOGFILE 
 VERIFY $? "installing mysql"
 
-dnf install git -y
+dnf install git -y | &>>$LOGFILE
 VERIFY $? "installing git"
 echo "is script proceeding?"
