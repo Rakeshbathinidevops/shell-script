fca72f741f12f84306cb5a65e94827f8dbf7c828
rakeshbathinidevops <rakeshbp2017@gmail.com>
2024-05-08 12:49:29 +0530

    message

--- 09-cinditions.sh
+++ 09-cinditions.sh
@@ -2,11 +2,10 @@
 AGE=$1
 read -p "enter you age:" AGE
 
-if [$AGE -gt 60]
+if [ $AGE -gt 60 ]
 then
-
-echo "you are not eligible"
+    echo "you are not eligible"
 
 else
-echo "you are eligible"
+    echo "you are eligible"
 fi
