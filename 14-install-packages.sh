735216138c25531be6c1da83e7abea71ffe1ef9a
rakeshbathinidevops <rakeshbp2017@gmail.com>
2024-05-09 12:12:24 +0530

    message

---
+++ 14-install-packages.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+    echo -e "$2 is $R FAILED $N"
+    exit1
+else
+    echo -e "$2 is $G SUCCESSFUL $N"
+fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "You are not a Super-user"
+    exit 1
+else    
+    echo "You are a Super-user"
+
+
+for i in $@
+do
+    echo " Installing $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0]
+    then
+        echo -e "$i already installd.. $Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "installation of $i"
+    fi
+done
