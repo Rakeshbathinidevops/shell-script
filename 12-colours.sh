db585a1f6b8e1cdb6f9af72a286accb36574a716
rakeshbathinidevops <rakeshbp2017@gmail.com>
2024-05-09 10:43:58 +0530

    message

---
+++ 12-colours.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $O | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VERIFY(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failure $N"
+        exit 1
+    else
+        echo -e "$2 is $G successful $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "run with root access"
+    exit1
+else
+    echo "you are super user"
+
+fi
+
+dnf install mysql -y &>>$LOGFILE 
+VERIFY $? "installing mysql"
+
+dnf install git -y &>>$LOGFILE
+VERIFY $? "installing git"
+
+dnf install dockerr -y &>>$LOGFILE
+VERIFY $? "installing docker"
+
+echo "is script proceeding?"
